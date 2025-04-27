fda07d433c5847281571ee51400a1987a852ae37
Valentina Zangirolami <valentina.zangirolami@gmail.com>
2025-04-27 02:42:30 +0200

    ex4 updates

--- decision_trees_ensemble/decision_trees.R
+++ decision_trees_ensemble/decision_trees.R
@@ -65,13 +65,121 @@ rt_work <-
 
 trees_cv_results <- fit_resamples(rt_work, folds)
 trees_fit_1 <- fit(rt_work, train_data)
-collect_metrics(trees_cv_results) 
+
 # Get predictions on training data
 train_preds <- predict(trees_fit_1, new_data = train_data) %>% 
   bind_cols(train_data)  # Attach true values
+# Get predictions on validation data
+val_preds <- predict(trees_fit_1, new_data = val_data) %>% 
+  bind_cols(val_data)  # Attach true values
 
 # Compute metrics (e.g., RMSE, R-squared for regression)
 metrics <- metric_set(rmse, rsq)
 train_metrics <- metrics(train_preds, truth = MarketCap, estimate = .pred)
+val_metrics <- metrics(val_preds, truth = MarketCap, estimate = .pred)
 
-train_metrics
+round(train_metrics$.estimate, 4)
+round(val_metrics$.estimate,4)
+
+#new hyperparameter for cost complexity
+
+tree_spec <- decision_tree(cost_complexity = 0.1, tree_depth=30, min_n=20) |>
+  set_engine("rpart") |> set_mode("regression")
+
+rt_work <- 
+  workflow() |>
+  add_model(tree_spec) |> 
+  add_recipe(esg_recipe)
+
+tree_spec <- decision_tree(cost_complexity = 0.1, tree_depth=30, min_n=20) |>
+  set_engine("rpart") |> set_mode("regression")
+
+rt_work <- 
+  workflow() |>
+  add_model(tree_spec) |> 
+  add_recipe(esg_recipe)
+
+trees_fit_2 <- fit(rt_work, train_data)
+
+# Get predictions on training data
+train_preds <- predict(trees_fit_2, new_data = train_data) %>% 
+  bind_cols(train_data)  # Attach true values
+# Get predictions on validation data
+val_preds <- predict(trees_fit_2, new_data = val_data) %>% 
+  bind_cols(val_data)  # Attach true values
+
+# Compute metrics (e.g., RMSE, R-squared for regression)
+metrics <- metric_set(rmse, rsq)
+train_metrics <- metrics(train_preds, truth = MarketCap, estimate = .pred)
+val_metrics <- metrics(val_preds, truth = MarketCap, estimate = .pred)
+
+round(train_metrics$.estimate, 4)
+round(val_metrics$.estimate,4)
+
+#grid_search --> don't run!
+
+tree_spec <- decision_tree(cost_complexity = 0.01, tree_depth=tune(), min_n=tune()) |>
+  set_engine("rpart") |> set_mode("regression")
+
+tree_grid <- grid_regular(
+  tree_depth(),
+  min_n(),
+  levels = 20  # Number of values for each parameter
+)
+
+rt_work <- 
+  workflow() |>
+  add_model(tree_spec) |> 
+  add_recipe(esg_recipe)
+
+
+tree_res <- tune_grid(
+  rt_work,
+  resamples = folds,
+  grid = tree_grid,
+  metrics = metric_set(rmse, rsq)  # Choose appropriate metrics
+)
+
+best_tree <- tree_res |> select_best("rmse")
+
+final_wf <- tree_wf |>  
+  finalize_workflow(best_tree)
+
+final_fit <- final_wf |> 
+  fit(train_data)
+
+# Get predictions on training data
+train_preds <- predict(final_fit, new_data = train_data) %>% 
+  bind_cols(train_data)  # Attach true values
+# Get predictions on validation data
+val_preds <- predict(final_fit, new_data = val_data) %>% 
+  bind_cols(val_data)  # Attach true values
+
+# Compute metrics (e.g., RMSE, R-squared for regression)
+metrics <- metric_set(rmse, rsq)
+train_metrics <- metrics(train_preds, truth = MarketCap, estimate = .pred)
+val_metrics <- metrics(val_preds, truth = MarketCap, estimate = .pred)
+
+round(train_metrics$.estimate, 4)
+round(val_metrics$.estimate,4)
+
+#plot first tree
+
+final_model <- extract_fit_engine(trees_fit_1)
+
+rpart.plot(
+  final_model,
+  type = 4,
+  extra = 101,
+  box.palette = "Blues",
+  nn = TRUE,
+  fallen.leaves = FALSE,
+  shadow.col = "gray"
+)
+
+#feature importance
+
+vip(final_model, geom = "point", horizontal = FALSE) +
+  ggtitle("Decision Tree Feature Importance") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
--- decision_trees_ensemble/ensemble_methods.R
+++ decision_trees_ensemble/ensemble_methods.R
@@ -10,10 +10,130 @@
 
 #load libraries
 library(car)
+library(vip)
 library(ggplot2)
 library(tidymodels)
+library(baguette)
 library(tidyverse)
 
 esg_data <- read.csv("esg_financial_dataset.csv")
 head(esg_data)
+
+#split train-val
+
+set.seed(123)  # For reproducibility
+split <- initial_split(esg_data, prop = 0.8)
+
+train_data <- training(split)
+val_data <- testing(split)
+
+#bagging
+
+# Create recipe
+bag_recipe <- recipe(MarketCap ~ ., data = train_data) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+# Create bagging model
+bag_spec <- bag_tree(cost_complexity = 0.01, tree_depth = 30, min_n = 20) %>%
+  set_engine("rpart", times = 50) %>%
+  set_mode("regression")
+
+# Create workflow
+bag_wf <- workflow() %>%
+  add_recipe(bag_recipe) %>%
+  add_model(bag_spec)
+
+# Fit model
+bag_fit <- fit(bag_wf, data = train_data)
+
+# Get predictions on training data
+train_preds <- predict(bag_fit, new_data = train_data) %>% 
+  bind_cols(train_data)  # Attach true values
+# Get predictions on validation data
+val_preds <- predict(bag_fit, new_data = val_data) %>% 
+  bind_cols(val_data)  # Attach true values
+
+# Compute metrics (e.g., RMSE, R-squared for regression)
+metrics <- metric_set(rmse, rsq)
+train_metrics <- metrics(train_preds, truth = MarketCap, estimate = .pred)
+val_metrics <- metrics(val_preds, truth = MarketCap, estimate = .pred)
+
+round(train_metrics$.estimate, 4)
+round(val_metrics$.estimate,4)
+
+#random forest
+
+rf_spec <- rand_forest(mtry = 8, trees = 10, min_n = 8) %>%
+  set_engine("ranger", importance = "impurity") %>%
+  set_mode("regression")
+
+# Create workflow
+rf_wf <- workflow() %>%
+  add_recipe(esg_recipe) %>%
+  add_model(rf_spec)
+
+# Fit model
+rf_fit <- fit(rf_wf, data = train_data)
+
+# Get predictions on training data
+train_preds <- predict(rf_fit, new_data = train_data) %>% 
+  bind_cols(train_data)  # Attach true values
+# Get predictions on validation data
+val_preds <- predict(rf_fit, new_data = val_data) %>% 
+  bind_cols(val_data)  # Attach true values
+
+# Compute metrics (e.g., RMSE, R-squared for regression)
+metrics <- metric_set(rmse, rsq)
+train_metrics <- metrics(train_preds, truth = MarketCap, estimate = .pred)
+val_metrics <- metrics(val_preds, truth = MarketCap, estimate = .pred)
+
+round(train_metrics$.estimate, 4)
+round(val_metrics$.estimate,4)
+
+# reduce complexity
+
+rf_spec <- rand_forest(mtry = 8, trees = 6, min_n = 8) %>%
+  set_engine("ranger", importance = "impurity") %>%
+  set_mode("regression")
+
+# Create workflow
+rf_wf <- workflow() %>%
+  add_recipe(esg_recipe) %>%
+  add_model(rf_spec)
+
+# Fit model
+rf_fit <- fit(rf_wf, data = train_data)
+
+# Get predictions on training data
+train_preds <- predict(rf_fit, new_data = train_data) %>% 
+  bind_cols(train_data)  # Attach true values
+# Get predictions on validation data
+val_preds <- predict(rf_fit, new_data = val_data) %>% 
+  bind_cols(val_data)  # Attach true values
+
+# Compute metrics (e.g., RMSE, R-squared for regression)
+metrics <- metric_set(rmse, rsq)
+train_metrics <- metrics(train_preds, truth = MarketCap, estimate = .pred)
+val_metrics <- metrics(val_preds, truth = MarketCap, estimate = .pred)
+
+round(train_metrics$.estimate, 4)
+round(val_metrics$.estimate,4)
+
+# feature importance
+
+final_model <- extract_fit_engine(rf_fit)
+
+vip(final_model, geom = "point", horizontal = FALSE) +
+  ggtitle("Decision Tree Feature Importance") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+#exercise
+
+# Analyze the dataset london_bike_sharing from e-learning and apply 
+# appropriate statistical learning while finding good hyperparameters whether it is possible.
+
+london_data <- read.csv("london_bike_sharing.csv")
+head(london_data)
 
